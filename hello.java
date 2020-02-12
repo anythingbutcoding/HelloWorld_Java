4cc2c4329372897b4ca07b755454f6e02e145000
anythingbutcoding <60853633+anythingbutcoding@users.noreply.github.com>
2020-02-13 02:14:13 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -7,7 +7,7 @@ public static void main (String args[])
 {
 
 System.out.println("Java Hello World, this was my first program, it should trigger on commit");
-System.out.println("Let see if it worksssss");
+System.out.println("Let see if it workssssss");
 }
 
 }
