2a2865a5c6280e89e2af54aee5374bec10bdf727
anythingbutcoding <60853633+anythingbutcoding@users.noreply.github.com>
2020-02-13 19:15:41 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -8,7 +8,7 @@ public static void main (String args[])
 
 System.out.println("Java Hello World, this was my first program, it should trigger on commit");
 System.out.println("Let see if it workssssss");
-System.out.println("This will build mutiple jobs at onces");
+System.out.println("This will build mutiple jobs at onces, pipeline");
 }
 
 }
